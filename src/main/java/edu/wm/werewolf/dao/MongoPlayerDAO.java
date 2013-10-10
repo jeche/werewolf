f8d0b35f41d3fca278a2b3710a2c43e27c9b488d
jeche <jlchen@email.wm.edu>
2013-10-09 21:11:56 -0400

    players that are dead no longer included in geospatialqueries

--- src/main/java/edu/wm/werewolf/dao/MongoPlayerDAO.java
+++ src/main/java/edu/wm/werewolf/dao/MongoPlayerDAO.java
@@ -58,9 +58,7 @@ public class MongoPlayerDAO implements IPlayerDAO {
 	
 	@Override
 	public void update(Player updated) throws NoPlayerFoundException {
-		// TODO Auto-generated method stub
 		getPlayerByID(updated.getId());
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		BasicDBObject document = new BasicDBObject();
 		document.put("_id", updated.getId());
@@ -69,10 +67,8 @@ public class MongoPlayerDAO implements IPlayerDAO {
 		document.put("votedAgainst", updated.getVotedAgainst());
 		document.put("isWerewolf", updated.isWerewolf());
 		document.put("isDead", updated.isDead());
-		if(updated.isDead()) {
-			System.out.println("CRAPPPPP" + updated.getId().toString());
-		}
 		table.save(document);
+		// Used for indexing to allow for geospatial queries.
 		DBObject index2d = BasicDBObjectBuilder.start("loc", "2d").get();
 		System.out.println(index2d);
 		table.ensureIndex(index2d);
@@ -80,8 +76,6 @@ public class MongoPlayerDAO implements IPlayerDAO {
 
 	@Override
 	public void createPlayer(Player player) {
-		// TODO Auto-generated method stub
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		BasicDBObject document = new BasicDBObject();
 		document.put("_id", player.getId());
@@ -98,7 +92,6 @@ public class MongoPlayerDAO implements IPlayerDAO {
 
 	@Override
 	public Player getPlayerByID(String id) throws NoPlayerFoundException {
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		BasicDBObject query = new BasicDBObject("_id", id);
 		DBObject cursor = table.findOne(query);
@@ -110,8 +103,6 @@ public class MongoPlayerDAO implements IPlayerDAO {
 	
 	@Override
 	public List<Player> getAllPlayers() {
-		// TODO Auto-generated method stub
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		Player alive;
 		List<Player> players = new ArrayList<>();
@@ -129,40 +120,8 @@ public class MongoPlayerDAO implements IPlayerDAO {
 		return players;
 	}
 
-	/********For Testing Purposes Only*********/
-	/**
-	 * @return the mongo
-	 */
-	public MongoClient getMongo() {
-//		return mongo;
-		return null;
-	}
-
-	/**
-	 * @param mongo the mongo to set
-	 */
-	public void setMongo(MongoClient mongo) {
-//		this.mongo = mongo;
-	}
-
-	/**
-	 * @return the db
-	 */
-	public DB getDb() {
-		return db;
-	}
-
-	/**
-	 * @param db the db to set
-	 */
-	public void setDb(DB db) {
-		this.db = db;
-	}
-
 	@Override
 	public List<Player> nearPlayers(Player player, double distance) {
-		// TODO Auto-generated method stub
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		BasicDBList v1 = new BasicDBList();
 		v1.add(player.getLng());
@@ -178,7 +137,9 @@ public class MongoPlayerDAO implements IPlayerDAO {
 		while (cursor.hasNext()) {
 			DBObject item = cursor.next();
 			alive = new Player((String)item.get("_id"), (boolean) item.get("isDead"),(double) ((BasicDBList)item.get("loc")).get(1), (double) ((BasicDBList)item.get("loc")).get(0), (String) item.get("userId"),(boolean) item.get("isWerewolf"));
-			players.add(alive);
+			if(!alive.isDead()) {
+				players.add(alive);
+			}
 		}
 		}
 		finally {
@@ -189,14 +150,12 @@ public class MongoPlayerDAO implements IPlayerDAO {
 
 	@Override
 	public void clearPlayers() {
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		table.drop();
 	}
 
 	@Override
 	public int numWolves() {
-//		db = mongo.getDB("werewolf");
 		DBCollection table = db.getCollection("Player");
 		BasicDBObject query = new BasicDBObject("isWerewolf", true);
 		DBCursor cursor = table.find(query);
